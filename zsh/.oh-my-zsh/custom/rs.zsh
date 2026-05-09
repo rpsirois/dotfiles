68f7bb43982a1e5726bae46cab83a63381f8ac11
Robert Sirois <rpsirois@gmail.com>
2026-05-08 22:56:32 -0400

    moved out sensitive zsh config and added ghostty tilix shortcuts

--- zsh/.oh-my-zsh/custom/rs.zsh
+++ zsh/.oh-my-zsh/custom/rs.zsh
@@ -1,18 +1,32 @@
 # zshrc
 
+# -----------------------------------------------
+# terminal stuffs
+# -----------------------------------------------
+# include dotfiles in tab completion
+setopt GLOB_DOTS
+
+export LSCOLORS=GxFxCxDxBxegedabagaced
+
+# -----------------------------------------------
+# alias stuffs
+# -----------------------------------------------
 alias ls="ls -aG"
 alias ll="ls -lh"
 alias grep="grep --color=auto"
 
-alias vps_u="ssh a@b -i ~/.ssh/c"
-alias vps_xz="ssh a@b -i ~/.ssh/c"
-alias vps_null="ssh a@b -i ~/.ssh/c"
+# include headers and use column format in sqlite3 shell
+alias sqlite3="sqlite3 -header -column"
 
-#unalias gau
-
-export LSCOLORS=GxFxCxDxBxegedabagaced
-
+# tee a log from the ssh session locally
 tssh() { ssh "$@" | tee "ssh_session_$(date +%Y%m%dT%H%M%S)"; }
+
+# -----------------------------------------------
+# specifc tech stack stuffs
+# -----------------------------------------------
+
+export PATH="$HOME/go/bin:$PATH"
+#unalias gau
 
 export NVM_DIR="$HOME/.nvm"
 source $(brew --prefix nvm)/nvm.sh
