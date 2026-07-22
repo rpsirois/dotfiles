391652adaf2bb694295481d73b61783d6d5bfbdc
Robert Sirois <rpsirois@gmail.com>
2026-07-22 09:49:34 -0600

    added fugitive and lightline plugins. updated l/ls/ll/lt etc. aliases and coloration for BSD vs. Linux

--- vim/.vimrc
+++ vim/.vimrc
@@ -2,8 +2,17 @@ set nocompatible
 syntax enable
 filetype off
 
+let mapleader = ","
+
 map <C-n> :NERDTreeToggle<CR>
 map <C-f> :NERDTreeFind<CR>
+
+" show dotfiles: netrw when invoked directly, NERDTree when it hijacks a directory buffer (e.g. "vim .")
+let g:netrw_hide = 0
+let g:NERDTreeShowHidden = 1
+
+" cwd to current buffer's directory
+nnoremap <leader>cd :lcd %:h<CR>
 
 " so that vim thinks *.md is markdown and not Modula-2
 autocmd BufNewFile,BufReadPost *.md set filetype=markdown
@@ -21,6 +30,9 @@ set hlsearch
 hi Search guibg=LightBlue
 set et sw=2 ts=2 sts=2 encoding=utf-8
 set nowrap
+set number
+set wildmenu
+set noswapfile nobackup
 
 vnoremap < <gv
 vnoremap > >gv
--- zsh/.oh-my-zsh/custom/rs.zsh
+++ zsh/.oh-my-zsh/custom/rs.zsh
@@ -6,13 +6,26 @@
 # include dotfiles in tab completion
 setopt GLOB_DOTS
 
+# BSD/macOS ls -G colors; dir changed from default bold blue to bold cyan for readability
 export LSCOLORS=GxFxCxDxBxegedabagaced
+# GNU ls --color equivalent of the LSCOLORS above
+export LS_COLORS="di=01;36:ln=01;35:so=01;32:pi=01;33:ex=01;31:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=34;43"
 
 # -----------------------------------------------
 # alias stuffs
 # -----------------------------------------------
-alias ls="ls -aG"
-alias ll="ls -lh"
+# recursive expansion is intentional (ll inherits ls, etc.)
+# ls color flag differs by OS: GNU/Linux uses --color=auto; BSD/macOS uses -G.
+if [[ "$OSTYPE" == darwin* ]]; then
+  alias ls="ls -AG"               # BSD / macOS
+else
+  alias ls="ls -A --color=auto"   # GNU / Linux
+fi
+
+alias l="ls -lh"                  # mirror ll (override omz's default [ls -lah])
+alias ll="ls -lh"                 # long, human sizes (inherits -A + color)
+alias lt="ls -lht"                # long, sorted by mtime, newest first
+alias ltr="ls -lhtr"              # long, sorted by mtime, newest last
 alias grep="grep --color=auto"
 
 # include headers and use column format in sqlite3 shell
